4010ade70ab2b570f31625b28221d1c682b0a1cd
Maurice <Maurice@192.168.0.7>
2018-05-01 14:02:51 -0400

    updated still working. has error with buffer for 3 player game

---
+++ src/GUI/newGameHandler3.java
@@ -0,0 +1,24 @@
+package GUI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import code.Board;
+
+public class newGameHandler3 implements ActionListener {
+	
+	private Board _b;
+	private int g;
+	
+	public newGameHandler3(int q,Board b) {
+		_b=b;
+		g=q;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		_b.playerSet(g);
+		
+	}
+
+}
